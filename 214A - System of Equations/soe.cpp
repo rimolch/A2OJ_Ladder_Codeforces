acb90bbdc42db8f0080208599475150f473f400f
rimolch <67545874+rimolch@users.noreply.github.com>
2022-08-16 08:30:40 +0600

    Create soe.cpp

---
+++ 214A - System of Equations/soe.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int ans=0;
+    for(int a=0;a*a<=n&&a<=m;a++){
+       int  b=n-a*a;
+        if(a+(b*b)==m){
+           ans++;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
