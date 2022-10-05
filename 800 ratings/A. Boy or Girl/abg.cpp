b2004212294750ed9bea78a88db97d5ccc2c3007
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-05 12:58:43 +0600

    Create abg.cpp

---
+++ 800 ratings/A. Boy or Girl/abg.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    string s;
+    int i,j,count=0;
+    cin>>s;
+    sort(s.begin(),s.end());
+    for(i=0;i<s.size();i++)
+    {
+           if(s[i]!=s[i+1])
+           {
+               count+=1;
+           }
+          /* else
+           {
+               count+=1;
+           }*/
+    }
+    if(count%2==0)
+    {
+        cout<<"CHAT WITH HER!";
+    }
+    else
+    {
+        cout<<"IGNORE HIM!";
+    }
+    return 0;
+}
+ 
