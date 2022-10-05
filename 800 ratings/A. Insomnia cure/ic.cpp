423f43e31107343993782480a218d3c268ef8de6
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-05 12:49:30 +0600

    Create ic.cpp

---
+++ 800 ratings/A. Insomnia cure/ic.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+#define checkmate return 0;
+using namespace std;
+ 
+int main(){
+    int k,l,m,n,d,c=0;
+    cin>>k>>l>>m>>n>>d;
+    for(int i=1;i<=d;i++){
+        if(i%k==0||i%l==0||i%m==0||i%n==0){
+           c++;
+        }
+    }
+    cout << c;
+    checkmate
+}
