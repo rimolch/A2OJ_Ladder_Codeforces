5ed7e937aa765e15b3af0cf05ea68a816f6f6981
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-06 10:11:40 +0600

    Create ar.cpp

---
+++ 800 ratings/A. Reconnaissance 2/ar.cpp
@@ -0,0 +1,26 @@
+<a href="#include<bits/stdc++.h>
+ 
+using namespace std;
+int ar[1001];
+int main(){
+    int n;
+    cin>>n;
+     for(int i=0;i<n;i++){
+        cin>>ar[i];
+     }
+     int mini=INT_MAX;
+     int i,t1,t2;
+     for(i=0;i<n-1;i++){
+       if(abs(ar[i]-ar[i+1])<mini){
+          mini=abs(ar[i]-ar[i+1]);
+            t1=i+1;
+            t2=i+2;
+        }
+     }
+     if(abs(ar[0]-ar[n-1])<mini){
+       t1=1;
+       t2=n;
+     }
+     cout<<t1<<" "<<t2;
+     return 0;
+}">Problem</a>
