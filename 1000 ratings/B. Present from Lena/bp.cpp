c201876f188a3b62cfc7d24b0636b1fe235c1e12
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-06 17:31:44 +0600

    Create bp.cpp

---
+++ 1000 ratings/B. Present from Lena/bp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    for(int r=-n;r<=n;r++){
+        int top=n-abs(r);
+        for(int i=0;i<abs(r);i++){
+            cout<<"  ";
+        }
+        for(int i=0;i<top;i++){
+            cout<<i<<" ";
+        }
+        for(int i=top;i>0;i--){
+            cout<<i<<" ";
+        }
+        cout<<0<<endl;
+    }
+    return 0;
+}
