b9abd61487021b54d060478f5f41fb6380a58107
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-09 02:00:15 +0600

    Create bhs.cpp

---
+++ 1200 ratings/B. Hungry Sequence/bhs.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define MOD 1e7
+#define ll long long int
+using namespace std;
+
+int main(){
+	ll n;
+	cin>>n;
+	for(ll i=1;i<=n;i++){
+		cout<<(i+20*n)<<" ";//or we can use i+10*n
+	}
+	return 0;
+
+}
