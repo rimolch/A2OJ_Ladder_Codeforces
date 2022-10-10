3b9374f0eec3aafe302e0e83dd4647a666b98af8
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-10 15:10:15 +0600

    Update ayap.cpp

--- 1100 ratings/A. Yaroslav and Permutations/ayap.cpp
+++ 1100 ratings/A. Yaroslav and Permutations/ayap.cpp
@@ -6,19 +6,20 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin>>n;
-    int ar[1005]={0};
-    int is_ok =0;
-    int i;
-    for(i=0;i<n;i++){
-          int a;
-          cin>>a;
-          ar[a]++;
-          if(ar[a]>(n+1)/2){
-              break;
-          }
-    }
-    puts(i==n?"YES":"NO");
-    return 0;
+	//1 1 1 1 2 2 3 -->> 1 2 1 2 1 3 1
+	int n;
+	cin>>n;
+	map<int,int>mp;
+	for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        mp[x]++;
+	}
+	int maxi =-1;
+	for(int i=0;i<mp.size();i++){
+        maxi =max(maxi,mp[i]);
+	}
+	if(2*maxi-1<=n)cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	return 0;
 }
