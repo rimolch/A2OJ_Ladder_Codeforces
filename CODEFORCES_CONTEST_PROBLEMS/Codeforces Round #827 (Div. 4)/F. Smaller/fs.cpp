28c8b34e093ada37c9ec59dafbdfeeb619986dd2
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-14 18:35:15 +0600

    Update fs.cpp

--- CODEFORCES_CONTEST_PROBLEMS/Codeforces Round #827 (Div. 4)/F. Smaller/fs.cpp
+++ CODEFORCES_CONTEST_PROBLEMS/Codeforces Round #827 (Div. 4)/F. Smaller/fs.cpp
@@ -35,3 +35,42 @@ int main(){
  	}
 	return 0;
 }
+/*
+#include <bits/stdc++.h>
+#define MOD 1073741824
+#define ll long long int
+#define endl '\n'
+using namespace std;
+ 
+int main(){
+     ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     int t;
+     cin>>t;
+	 while(t--){
+		ll q;
+		cin>>q;
+		bool flagA=0,flagB=0;
+		ll cntA=0,cntB =0;
+		while(q--){
+		   ll d,k;
+		   string s;
+		   cin>>d>>k>>s;
+		  for(int i=0;i<s.size();i++){
+			  if(d==1){
+				  if(s[i]>'a')flagA=1;
+				  else cntA+=k;
+			  }
+			  else{
+				  if(s[i]>'a')flagB=1;
+				  else cntB+=k;
+			  }
+		   }
+		   if(flagB==1)cout<<"YES"<<endl;
+		   else if(flagA==0&&cntA<cntB)cout<<"YES"<<endl;
+		   else cout<<"NO"<<endl;
+		}
+	 }
+	return 0;
+}
+*/
