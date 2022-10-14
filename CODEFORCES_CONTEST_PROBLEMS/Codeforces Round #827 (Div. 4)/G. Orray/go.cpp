0ac31d5dd0ed0019e58c1ae6d10fbb95499f187d
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-14 23:43:49 +0600

    Create go.cpp

---
+++ CODEFORCES_CONTEST_PROBLEMS/Codeforces Round #827 (Div. 4)/G. Orray/go.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define MOD 1073741824
+#define ll long long int
+#define endl '\n'
+using namespace std;
+
+int main(){
+     ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		vector<int> v(n);
+		for(int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		int curr = 0;
+		for(int i = 0; i < min(n, 30); i++) {
+			int temp = curr;
+			int ind = i;
+			for(int j = i; j < n; j++) {
+				if(temp < (curr | v[j])) {
+					temp = curr | v[j];
+					ind = j;
+				}
+			}
+			swap(v[ind], v[i]);
+			curr = temp;
+		}
+		for(int i = 0; i < n; i++) {
+			cout << v[i] << ' ';
+		}
+		cout << '\n';
+	}
+     return 0;
+}
