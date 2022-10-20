5e3b8c2230a359bead3f8ede4907f7c135975aa2
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-20 16:31:58 +0600

    Update bss.cpp

--- 1100 ratings/B - Sereja and Suffixes/bss.cpp
+++ 1100 ratings/B - Sereja and Suffixes/bss.cpp
@@ -2,21 +2,7 @@
 #define ll long long int
 #define MOD 1073741824
 using namespace std;
-bool is_prime(int n)
-{
-    if (n <= 1)
-        return false;
-    if (n <= 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
 
-    for (int i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-
-    return true;
-}
 int main() {
      ios_base::sync_with_stdio(0);
      cin.tie(0);
@@ -24,7 +10,6 @@ int main() {
      cin>>n>>m;
      set<ll>s;
      vector<ll>v;
-     vector<ll>b;
      for(int i=0;i<n;i++){
         cin>>x;
         v.push_back(x);
@@ -34,8 +19,8 @@ int main() {
         v[i]=s.size();
      }
      for(int i=0;i<m;i++){
-         cin>>y;
-         cout<<v[y-1]<<endl;
+        cin>>y;
+        cout<<v[y-1]<<endl;
      }
      return 0;
 }
